391049efd7d98cf9efaaa8925059301bae4793ad
carmona44 <daniel_95ca@hotmail.com>
2019-02-07 17:29:10 +0100

    Updates

---
+++ precache-manifest.3cdadb01b4bc33311915156c300ccce1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9e9cc015ed0d9f1c927",
+    "url": "/webs-interesantes/static/js/runtime~main.d9e9cc01.js"
+  },
+  {
+    "revision": "93dd1093a869dcf03567",
+    "url": "/webs-interesantes/static/js/main.93dd1093.chunk.js"
+  },
+  {
+    "revision": "2f3cfdeb48511d98f288",
+    "url": "/webs-interesantes/static/js/1.2f3cfdeb.chunk.js"
+  },
+  {
+    "revision": "93dd1093a869dcf03567",
+    "url": "/webs-interesantes/static/css/main.16fc5ded.chunk.css"
+  },
+  {
+    "revision": "7697325e0b4b4dac935e62e798e293b2",
+    "url": "/webs-interesantes/index.html"
+  }
+];
